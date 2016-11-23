cb931c331c4060744736ece90dc6902ef3f1f70f
mingyuan.pei@dianjoy.com <mingyuan.pei@dianjoy.com>
2016-11-23 15:24:01 +0800

    修改gunicorn配置

--- etc/gunicorn.py
+++ etc/gunicorn.py
@@ -22,6 +22,7 @@ workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'gunicorn.workers.ggevent.GeventWorker'
 
 loglevel = 'info'
+timeout = 90
 errorlog = '-'
 
 x_forwarded_for_header = 'X-FORWARDED-FOR'
