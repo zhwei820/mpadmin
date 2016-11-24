d96fc9d52c5f52eff26bde6ff099996041ec264f
马德岭 <lingnck@gmail.com>
2016-11-24 14:19:21 +0800

    优化项目架构

--- CMDB/settings.py
+++ CMDB/settings.py
@@ -135,21 +135,13 @@ import mongoengine
 SESSION_ENGINE = 'django_mongoengine.sessions'
 SESSION_SERIALIZER = 'django_mongoengine.sessions.BSONSerializer'
 
-_MONGODB_USER = 'mongotest'
-_MONGODB_PASSWD = 'mongotest'
-_MONGODB_HOST = '192.168.199.222'
-_MONGODB_NAME = 'mongotest'
-_MONGODB_DATABASE_HOST = \
-    'mongodb://%s:%s@%s/%s' \
-    % (_MONGODB_USER, _MONGODB_PASSWD, _MONGODB_HOST, _MONGODB_NAME)
-
 MONGODB_DATABASES = {
     "default": {
-        "name": _MONGODB_NAME,
-        "host": _MONGODB_HOST,
-        "password": _MONGODB_PASSWD,
-        "username": _MONGODB_USER,
-        "tz_aware": True, # if you using timezones in django (USE_TZ = True)
+        "name": 'mongotest',
+        "host": '192.168.199.222',
+        "password": 'mongotest',
+        "username": 'mongotest',
+        "tz_aware": True,  # if you using timezones in django (USE_TZ = True)
     },
 }
 
