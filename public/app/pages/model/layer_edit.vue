a13ea2247911f1c29975160eb1c10eec442820b2
zhouwei <602068971@qq.com>
2017-02-27 16:00:18 +0800

    + ui fix

--- public/app/pages/model/layer_edit.vue
+++ public/app/pages/model/layer_edit.vue
@@ -2,7 +2,7 @@
   <!--<el-dialog title="CI模型层编辑" v-model="dialogFormVisible">-->
   <el-row type="flex" class="row-bg" justify="center">
     <el-col :span="6">
-      <h2><span v-if="_layerId">编辑</span><span v-else>新建</span>CI模型层</h2>
+      <h2><span v-if="layerId_1">编辑</span><span v-else>新建</span>CI模型层</h2>
       <el-form :model="form" label-position="top">
         <el-form-item label="CI模型层名称" label-width="80">
           <el-input v-model="form.name" auto-complete="off"></el-input>
@@ -31,21 +31,20 @@
   export default {
     data() {
       return {
+        layerId_1: "",
         input2: "",
-
         form: {
           name: '',
           id: "",
         },
         _form: {},
         id: 0,
-        _layerId: "",
       }
     },
     props: ['layerId'],
     watch: {
       layerId: function (dest, src) {
-        this._layerId = this.layerId
+        this.layerId_1 = this.layerId
         this.fetch(0, 100)
       }
     },
@@ -60,8 +59,8 @@
         // var id = this.$route.params.id
         // this.id = id == undefined ? 0 : id
 
-        if (this._layerId) {
-          this.$http.get("/api/layers/" + this._layerId + "/?" + Date.now()).then((response) => {
+        if (this.layerId_1) {
+          this.$http.get("/api/layers/" + this.layerId_1 + "/?" + Date.now()).then((response) => {
             this.form = response.data
 
           }, (response) => {
@@ -81,7 +80,7 @@
             // this.$router.push({
             //   path: "/layer_edit/" + response.data.id
             // })
-            this._layerId = response.data.id
+            this.layerId_1 = response.data.id
             this.fetch(0, 100)
             parent.vm.show_ok_message("新建成功！")
 
